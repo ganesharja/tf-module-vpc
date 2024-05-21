82288c4cd646206b7159bd3692a5056b653c7738
Ganesh Arja <jayaganesharja.com>
2024-05-21 12:32:30 +0530

    Auto push

--- gw.tf
+++ gw.tf
@@ -9,8 +9,7 @@ resource "aws_internet_gateway" "igw" {
 
 # Creates Elastic IP Address which would used by the NAT GW. Ensure, EIP is provisioned first and then NAT
 resource "aws_eip" "ngw_ip" {
-  vpc      = true
-
+  
   tags = {
     Name = "roboshop-${var.ENV}-ngw-eip"
   }
